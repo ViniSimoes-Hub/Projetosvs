23de96fb4e4285dd118ea50ea7da25cda4f92776
Vinicius <viniciushard@icloud.com>
2024-02-27 15:30:32 -0300

    primeiro committ

--- teste.java
+++ teste.java
@@ -2,6 +2,8 @@
 public class teste {
 
     public static void main(String[] args) {
-        System.err.println("Hello");
+        System.err.println("Hello World");
+        System.err.println("Hello ");
+        System.err.println("Hello ");
     }
 }
